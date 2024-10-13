89bfe241e14cd5982fec9fee3cdae5729222d3eb
suhoco <118863847+suhoco@users.noreply.github.com>
2024-10-14 02:53:21 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241014/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    int d = a+b+c;
+
+    cout << d << endl << d/3 << endl << d-(d/3);
+    return 0;
+}
