2ca608bf7158ed9beba38d1ea3f73b20c3acc918
suhoco <118863847+suhoco@users.noreply.github.com>
2024-10-14 02:50:23 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241014/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+
+    cin >> a >> b;
+    
+    int c = a+b;
+
+    cout << c << " " << (double)c/2;
+    return 0;
+}
