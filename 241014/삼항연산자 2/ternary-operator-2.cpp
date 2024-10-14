3c5c295f666cf0ed11ad2221a6b9983025b67e77
suhoco <118863847+suhoco@users.noreply.github.com>
2024-10-14 10:14:40 +0900

    Title: 삼항연산자 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241014/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+
+    cin >> a;
+    
+    char b = (a == 1) ? 't' : 'f';
+
+    cout << b;
+    return 0;
+}
