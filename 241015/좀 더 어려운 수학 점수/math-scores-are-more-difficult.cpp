43e7ac857b4095b602a4ade2f65f34385855af71
suhoco <118863847+suhoco@users.noreply.github.com>
2024-10-15 11:09:22 +0900

    Title: 좀 더 어려운 수학 점수, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241015/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a_math, a_eng, b_math, b_eng;
+
+    cin >> a_math >> a_eng;
+    cin >> b_math >> b_eng;
+
+    if (a_math > b_math)
+        cout << 'A';
+    else if (a_math < b_math)
+        cout << 'B';
+    else if (a_eng > b_eng)
+        cout << 'A';
+    else
+        cout << 'B';
+    return 0;
+}
