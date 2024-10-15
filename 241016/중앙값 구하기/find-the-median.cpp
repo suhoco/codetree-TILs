175c2a824323fd3b2d08c583a40b486fa2aead62
suhoco <118863847+suhoco@users.noreply.github.com>
2024-10-16 03:53:00 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241016/중앙값 구하기/find-the-median.cpp
+++ 241016/중앙값 구하기/find-the-median.cpp
@@ -9,7 +9,7 @@ int main() {
 
     if ((b < a && a < c) || (c < a && a < b))
         cout << a;
-    else if ((b < c) || (b > c))
+    else if ((a < b && b < c) || (c < b && b < a))
         cout << b;
     else
         cout << c;
