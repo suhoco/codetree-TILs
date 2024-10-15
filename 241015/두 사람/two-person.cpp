77a29a060e2d67765398f3975101fddf1514c696
suhoco <118863847+suhoco@users.noreply.github.com>
2024-10-15 11:14:09 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241015/두 사람/two-person.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a_age, b_age;
+    char a_sex, b_sex;
+
+    cin >> a_age >> a_sex;
+    cin >> b_age >> b_sex;
+
+    cout << (a_age >= 19 && a_sex == 'M') || (b_age >= 19 && b_sex == 'M');
+    
+    return 0;
+}
