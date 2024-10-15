af9e2d7e855a56252fba72f13404b2d06074ddf7
suhoco <118863847+suhoco@users.noreply.github.com>
2024-10-16 00:18:57 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241016/윤년인가/is-leap-year.cpp
+++ 241016/윤년인가/is-leap-year.cpp
@@ -6,7 +6,7 @@ int main() {
     int y;
 
     cin >> y;
-
+/*
     if (y % 4 == 0){
         if (y % 100 == 0 && y % 400 != 0)
             cout << "false";
@@ -15,8 +15,12 @@ int main() {
     }
     else
         cout << "false";
+*/
 
-//    if (y % 4 == 0)
+    if ((y % 4 == 0) && (y % 100 != 0))
+        cout << "true";
+    else
+        cout << "false";
 
     return 0;
 }
