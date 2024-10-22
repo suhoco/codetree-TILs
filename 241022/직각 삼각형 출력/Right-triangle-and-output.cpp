82e2483ea6b7f52e52c76ab090b7b726db0e887c
suhoco <118863847+suhoco@users.noreply.github.com>
2024-10-22 12:25:36 +0900

    Title: 직각 삼각형 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241022/직각 삼각형 출력/Right-triangle-and-output.cpp
+++ 241022/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -7,8 +7,8 @@ int main() {
 
     cin >> n;
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j < (i*2-1); j++)
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < (i*2+1); j++)
             cout << "*";
         cout << endl;
     }
