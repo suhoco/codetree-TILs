22499a8536080c3e42e5669aaaf99396edaf6b5a
suhoco <118863847+suhoco@users.noreply.github.com>
2024-10-23 07:34:55 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/문자 출력하기/print-text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char a;
+
+    cin >> a;
+
+    for (int i = 0; i < 8; i++)
+        cout << a;
+    return 0;
+}
