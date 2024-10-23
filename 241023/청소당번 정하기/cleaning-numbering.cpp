ce39e72ddcdfbf766840f00f90f87aea81559164
suhoco <118863847+suhoco@users.noreply.github.com>
2024-10-23 09:08:31 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/청소당번 정하기/cleaning-numbering.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int cnt_class = 0, cnt_hall = 0, cnt_toilet = 0;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+
+        if (i % 12 == 0)
+            cnt_toilet++;
+        
+        else if (i % 6 == 0)
+            cnt_hall++;
+
+        else if (i % 3 == 0)
+            cnt_hall++;
+
+        else if (i % 2 == 0)
+            cnt_class++;
+    }
+
+    cout << cnt_class << " " << cnt_hall << " " << cnt_toilet;
+
+    return 0;
+}
