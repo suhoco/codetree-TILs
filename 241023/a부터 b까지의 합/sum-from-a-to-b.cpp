accb0864e18617cf4b0a5c3430bf64cde8c62cff
suhoco <118863847+suhoco@users.noreply.github.com>
2024-10-23 09:25:18 +0900

    Title: a부터 b까지의 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/a부터 b까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+
+    int sum = 0;
+
+    for (int i = a; i <= b; i++)
+        sum += i;
+
+    cout << sum;
+    
+    return 0;
+}
