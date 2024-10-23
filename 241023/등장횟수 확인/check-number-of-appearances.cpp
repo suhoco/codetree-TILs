6e30fa5d864b637647a417fca5dc38b1cf761d41
suhoco <118863847+suhoco@users.noreply.github.com>
2024-10-23 09:16:21 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int cnt = 0;
+
+    for (int i = 0; i < 5; i++) {
+        cin >> n;
+        if (n % 2 == 0)
+            cnt ++;
+    }
+
+    cout << cnt;
+    return 0;
+}
