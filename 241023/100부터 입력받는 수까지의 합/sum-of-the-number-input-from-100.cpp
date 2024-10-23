91468d300782cae318f382b95ebe393c393b67c2
suhoco <118863847+suhoco@users.noreply.github.com>
2024-10-23 09:26:51 +0900

    Title: 100부터 입력받는 수까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    
+    for (int i = n; i <= 100; i++)
+        sum += i;
+    
+    cout << sum;
+    
+    return 0;
+}
